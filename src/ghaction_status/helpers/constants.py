4100581246817dcb0b1e2c561fb58e6558fffe74
jylenhof <jygithub@lenhof.eu.org>
2026-04-18 10:40:13 +0200

    fix: miss some github action when there's no name but directly uses
    Signed-off-by: jylenhof <jygithub@lenhof.eu.org>

--- src/ghaction_status/helpers/constants.py
+++ src/ghaction_status/helpers/constants.py
@@ -3,12 +3,12 @@
 from pathlib import Path
 
 SEARCH_CONFIGS: list[tuple[Path, str, str]] = [
+    (Path(".github/workflows"), "*.yml", "- uses:"),
+    (Path(".github/workflows"), "*.yaml", "- uses:"),
     (Path(".github/workflows"), "*.yml", "uses:"),
     (Path(".github/workflows"), "*.yaml", "uses:"),
-    (
-        Path(".github/actions"),
-        "**/*.yml",
-        "uses:",
-    ),
+    (Path(".github/actions"),"**/*.yml","- uses:"),
+    (Path(".github/actions"), "**/*.yaml", "- uses:"),
+    (Path(".github/actions"),"**/*.yml","uses:"),
     (Path(".github/actions"), "**/*.yaml", "uses:"),
 ]
